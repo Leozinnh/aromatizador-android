dece40724f39a46419d4c7de841e5ad9351d4030
Leozinnh <leonardoaf65572005@gmail.com>
2025-09-23 22:08:49 -0300

    teste

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,8 +52,6 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
